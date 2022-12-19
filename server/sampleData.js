9f57462a273962d2a405e21e1752fd95ad5fdef4
Wilson <wruan@tvo.org>
2022-12-19 16:39:40 -0500

    graphql project

---
+++ server/sampleData.js
@@ -0,0 +1,72 @@
+const projects = [
+    {
+        id: '1',
+        clientID: '1',
+        name: 'eCommerce Website',
+        description: "whatever",
+        status: 'In Progress',
+    },
+    {
+        id: '2',
+        clientID: '2',
+        name: 'eCommerce Website',
+        description: "whatever",
+        status: 'In Progress',
+    },
+    {
+        id: '3',
+        clientID: '3',
+        name: 'eCommerce Website',
+        description: "whatever",
+        status: 'In Progress',
+    },
+    {
+        id: '4',
+        clientID: '4',
+        name: 'eCommerce Website',
+        description: "whatever",
+        status: 'In Progress',
+    },
+    {
+        id: '5',
+        clientID: '5',
+        name: 'eCommerce Website',
+        description: "whatever",
+        status: 'In Progress',
+    },
+]
+
+const clients = [
+    {
+        id: '1',
+        name: 'Wilson Ruan',
+        email: "whatever@gmail.com",
+        phone: '123-123-1234',
+    },
+    {
+        id: '2',
+        name: 'Wilson Ruan 2',
+        email: "whatever@gmail.com",
+        phone: '123-123-1234',
+    },
+    {
+        id: '3',
+        name: 'Wilson Ruan 3',
+        email: "whatever@gmail.com",
+        phone: '123-123-1234',
+    },
+    {
+        id: '4',
+        name: 'Wilson Ruan 4',
+        email: "whatever@gmail.com",
+        phone: '123-123-1234',
+    },
+    {
+        id: '5',
+        name: 'Wilson Ruan 5',
+        email: "whatever@gmail.com",
+        phone: '123-123-1234',
+    },
+]
+
+module.exports = { projects, clients };
