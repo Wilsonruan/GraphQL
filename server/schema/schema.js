dd5919a22b2b8a1724ded5f997eb8ed869cc185d
Wilson <wruan@tvo.org>
2022-12-20 14:38:22 -0500

    frontend started

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -138,10 +138,31 @@ const mutation = new GraphQLObjectType({
       type: ProjectType,
       args: {
         id: { type: new GraphQLNonNull(GraphQLID) },
-        name: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatusUpdate",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              completed: { value: "Completed" },
+            },
+          }),
+        },
       },
       resolve(parent, args) {
-        return Project.findByIdAndRemove(args.id);
+        return Project.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true }
+        );
       },
     },
   },
