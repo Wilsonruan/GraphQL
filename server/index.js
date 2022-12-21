69828d43f0a6c66498e2aa0e8c62bda32541b776
Wilson <wruan@tvo.org>
2022-12-21 11:27:43 -0500

    attend to deploy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const color = require("colors")
+const color = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const connectDB = require("./config/db");
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
